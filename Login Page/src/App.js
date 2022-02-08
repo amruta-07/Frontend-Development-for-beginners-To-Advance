ef0b61128f960fe3cc6229a670b290ef284a5565
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-02-09 00:00:45 +0530

    Add files via upload

--- Login Page/src/App.js
+++ Login Page/src/App.js
@@ -1,1 +1,12 @@
+import React from "react";
+import LoginPage from "./components/LoginPage/LoginPage";
 
+function App() {
+  return (
+    <div>
+       <LoginPage/>
+    </div>
+  );
+}
+
+export default App;
