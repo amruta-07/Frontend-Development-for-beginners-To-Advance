4c56cb02dadbf876d34c9686dec02a576534eaa9
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-03-02 23:26:55 +0530

    Add files via upload

---
+++ React JS Tutorial/Files/Dashboard.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+
+export class Dashboard extends Component {
+    render() {
+        return (
+            <div
+            style={{
+                flex:1,
+                marginLeft:"220px"
+            }}
+            >
+               
+                
+            </div>
+
+            
+        )
+    }
+}
+
+export default Dashboard
