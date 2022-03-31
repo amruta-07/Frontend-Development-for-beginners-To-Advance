805050a6de8d3416c319b13213125c792c9ae982
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-03-31 22:55:19 +0530

    Add files via upload

---
+++ React JS Tutorial/Files/script.js
@@ -0,0 +1,39 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 60){
+    document.querySelector('#scroll-top').classList.add('active');
+  }else{
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
+
+}
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut();
+
+
+
+<<<<<<< HEAD
+//class="d-inline-block align-text-top
+=======
+>>>>>>> b7aff23 (code commit)
